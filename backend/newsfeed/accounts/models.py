50b81ce6df5a240d343473c8c681e61fafdccb26
AminTalebi <talebi242@gmail.com>
2020-01-30 10:49:33 +0330

    add account models

--- backend/newsfeed/accounts/models.py
+++ backend/newsfeed/accounts/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    followers = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    bio = models.TextField(max_length=100, blank=True)
+    joined_at = models.DateTimeField(auto_now_add=True)
