b1a7f1b7f3b0b6de420c7d59fe03620b5d210847
AminTalebi <talebi242@gmail.com>
2020-02-03 19:28:10 +0330

    add channels views

--- backend/newsfeed/channels/serializers.py
+++ backend/newsfeed/channels/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 
-from channels.models import Channel
+from channels.models import Channel, ChannelAdmin
 
 
-class PostSerializer(serializers.ModelSerializer):
+class ChannelSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Channel
         fields = ['id', 'creator', 'created_at', 'description', 'rules', 'is_personal']
 
 
+class ChannelAdminSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ChannelAdmin
+        fields = '__all__'
--- backend/newsfeed/channels/views.py
+++ backend/newsfeed/channels/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from channels.serializers import ChannelSerializer, ChannelAdminSerializer
+from channels.models import Channel, ChannelAdmin
+
+
+class ChannelCreateView(generics.CreateAPIView):
+    serializer_class = ChannelSerializer
+
+
+class ChannelDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = ChannelSerializer
+    queryset = Channel.objects.all()
+
+
+class ChannelAdminCreateView(generics.CreateAPIView):
+    serializer_class = ChannelAdminSerializer
+
+
+class ChannelAdminDetailView(generics.RetrieveDestroyAPIView):
+    serializer_class = ChannelAdminSerializer
+    queryset = ChannelAdmin.objects.all()
